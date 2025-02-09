f775178a5efae5e1d6537e01e6f8a009071c17a8
Yalith Torres <yalithdev@gmail.com>
2025-02-09 14:03:23 -0400

    Footer - año actual

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,10 +1,11 @@
 import { Typography } from "../common/typography"
 
 export function Footer() {
+  const currentYear = new Date().getFullYear()
   return (
     <footer className="py-5 flex gap-2 justify-center border-t border-black">
       <Typography tag={"p"}>Made with Next.js ♥</Typography>
-      <Typography tag={"strong"}>2023</Typography>
+      <Typography tag={"strong"}>2023 - {currentYear}</Typography>
     </footer>
   )
 }
