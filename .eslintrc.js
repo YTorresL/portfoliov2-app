a067d6585ba0262c127e16e44758a195fad656c3
Yalith Torres <yalithdev@gmail.com>
2026-01-08 00:40:33 -0500

    Problema arreglado.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,37 @@
 module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
   extends: ["plugin:react/recommended", "standard", "prettier"],
-  overrides: [],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   plugins: ["react"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  ],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
